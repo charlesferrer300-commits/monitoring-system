47276c0b572fcfdba83b73aece08a1803804c971
RICHHACKER45 <joseolinares1443@gmail.com>
2026-08-28 11:58:39 +0800

    feat: scaffold Android project and populate asset icons and build configurations

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,8 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
-  appName: 'SmartHomeMonitor',
+  appId: 'com.smarthome.app',
+  appName: 'Smart Home App',
   webDir: 'www'
 };
 
